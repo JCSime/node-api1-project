b71f1b30b645bc5ec5c53011b02c75bec843e978
John S <johnc.sime@gmail.com>
2022-02-07 18:32:26 -0500

    created /user endpoint

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
@@ -7,5 +8,29 @@ server.get('/hello', (req, res)=>{
     res.json({ message: 'hello' })
 })
 
+server.get('/api/server/users', (req, res)=>{
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({ 
+            message: 'something bad happened',
+            error: err.message
+        })
+    })
+})
+
+// server.get('/api/server/users', async (req, res)=>{
+//     try {
+//         const users = await User.find()
+//         res.json(users)
+//     } catch (err) {
+//         res.status(500).json({ 
+//             message: 'something bad happened',
+//             error: err.message
+//         })
+//     }
+// })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
