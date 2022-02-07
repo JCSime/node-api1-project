fe01a4d8c6b9e7e931865cde048ce904538c10a5
John S <johnc.sime@gmail.com>
2022-02-07 18:06:55 -0500

    setup index.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/hello', (req, res)=>{
+    res.json({ message: 'hello' })
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
